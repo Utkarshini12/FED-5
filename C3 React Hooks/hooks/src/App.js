17e66cba3e634ff283b9fed79b3e04f531877dae
Utkarshini12 <50311757+Utkarshini12@users.noreply.github.com>
2022-08-05 22:55:10 +0530

    tic tac toe application

--- C3 React Hooks/hooks/src/App.js
+++ C3 React Hooks/hooks/src/App.js
@@ -2,26 +2,108 @@
 import './App.css';
 import Counter from './concepts/State';
 import {Row, Col, Button, Container, Card, CardBody} from 'reactstrap'
+import {ToastContainer, toast} from 'react-toastify'
 import {useState} from 'react'
+import Icon from './components/Icon';
 
 const itemArray = new Array(9).fill("empty")
 function App() {
-  const [isCross, setIsCross] = useState(true)
+  const [isCross, setIsCross] = useState(true);
+  const [winMessage, setWinMessage] = useState("")
+
+
+  // -> reloadGame --> set all state values to their initial state 
+
+  const reloadGame = () => {
+    setIsCross(true); 
+    setWinMessage(""); 
+    itemArray.fill("empty", 0, 8); 
+
+  }
+
+
+
+  /*
+  // CHECK THE WINNER
+-> horizontal lines with same icon--> 0,1,2  3,4,5  6,7,8
+-> vertical lines with same icon -->  0,3,6  1,4,7  2,5,8
+-> diagonal lines wth same icon -->   0,4,8  2,4,6
+-> true -> declare the winner  --> winMessage
+  */
+
+
+const checkIsWinner =() => {
+
+  if(
+    itemArray[0] === itemArray[1] && itemArray[1] === itemArray[2] && itemArray[2] !== "empty"
+  ) {
+      setWinMessage(`${itemArray[0]} won!`)
+  } else if( itemArray[3] === itemArray[4] && itemArray[4] === itemArray[5] && itemArray[3] !== "empty") {
+    setWinMessage(`${itemArray[3]} won!`)
+  }
+
+
+}
+
+
+  /*
+  // CHANGEITEM  --> index 
+-> box should be empty 
+    -> cross / circle turn 
+-> not empty 
+    -> alert "already filled" 
+-> checkWinner
+  */
+
+
+
+
+// box 7 
+const changeItem = itemNumber => {
+
+  if(itemArray[itemNumber] === "empty") {
+    // this is telling us that whose turn it is 
+      itemArray[itemNumber] = isCross ? "cross" : "circle"
+      // switching the turns 
+      setIsCross(!isCross);
+  } else {
+    return toast("already filled" , {type: "error"})
+  }
+
+  checkIsWinner()
+
+} 
+
+
   return (
     <div className="bg vh-100">
+      <ToastContainer position='bottom-center' />
+
+    
+    
+    
+      
    <Container className='p-5'>
     <Row>
       <Col md={6} className='offset-md-3'>
+      {winMessage ? (
+          <div className='text-center m-3'>
+           <h1 className='text-success text-center'>{winMessage}</h1>
+         <Button color="success" className='text-center' onClick={reloadGame}>Reload the Game</Button>
+          </div>
+      ) : (
         <h1 className='text-warning text-center'>
-          {/* cross turn / circle turn */}
-          {isCross ? "cross" : "circle"} turn
-        </h1>
+        {/* cross turn / circle turn */}
+        {isCross ? "cross" : "circle"} turn
+      </h1>
+      )}
+       
 
         <div className='grid'>
-          {itemArray.map(()=> (
-            <Card color="warning">
+          {itemArray.map((item, index)=> (
+            <Card color="warning" key={index} onClick={()=> changeItem(index)}>
               <CardBody className='box'>
-                X
+                <Icon name={item} />
               </CardBody>
 
             </Card>
@@ -39,3 +121,15 @@ function App() {
 }
 
 export default App;
+
+
+// Turns --> state value --> isCross
+
+/*
+
+
+
+
+*/
+
+
--- C3 React Hooks/hooks/src/components/Icon.js
+++ C3 React Hooks/hooks/src/components/Icon.js
@@ -1,10 +1,16 @@
 // if the box is empty ==> pen , if cross => cross icons , if circle => circle icon 
 import {FaRegCircle, FaTimes, FaPen} from 'react-icons/fa'
 
-const Icon = (props) => {
-   switch(props.name) {
+const Icon = ({name}) => {
+   switch(name) {
     case "circle" : 
-    return <FaRegCircle className='icons' />
+    return <FaRegCircle className='icons' />; 
+    case "cross" :
+    return <FaTimes className='icons' />
+    default : 
+    return <FaPen className='icons' />
    
    }
 }
+
+export default Icon; 
